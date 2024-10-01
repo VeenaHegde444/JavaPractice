15c29e941ca646a7669b7ecfa5d715df10973d62
Veena Hegde <veena444@gmail.com>
2024-10-01 18:27:23 +0530

    demo page added

---
+++ src/gitPractice/DemoPage.java
@@ -0,0 +1,13 @@
+package gitPractice;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int sum = x + y;
+		System.out.println("Sum is: "+sum);
+
+	}
+
+}
